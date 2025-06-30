9ca2d22f77b0bf0b9aa80c811789c7ef7617e601
MAC MINI <patildinesh.h@gmail.com>
2025-06-30 14:53:00 +0530

    HighestoccureceChar

---
+++ src/practicalquestion/HighestOccuranceCharCount.java
@@ -0,0 +1,41 @@
+package practicalquestion;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HighestOccuranceCharCount {
+
+	public static void main(String[] args) {
+
+		String str = "tommorow";
+
+		printHighestCharater(str);
+	}
+
+	public static void printHighestCharater(String str) {
+
+		HashMap<Character, Integer> highestchar = new HashMap<>();
+
+		for (char ch : str.toCharArray()) {
+
+			if (Character.isWhitespace(ch))
+				continue;
+			highestchar.put(ch, highestchar.getOrDefault(ch, 0) + 1);
+		}
+
+		char maxChar = ' ';
+		int maxCount = 0;
+
+		for (Map.Entry<Character, Integer> entry : highestchar.entrySet()) {
+
+			if (entry.getValue() > maxCount) {
+				maxChar = entry.getKey();
+				maxCount = entry.getValue();
+			}
+		}
+
+		System.out.println("Character with highest occurrence: '" + maxChar + "' occurred " + maxCount + " times.");
+
+	}
+
+}
